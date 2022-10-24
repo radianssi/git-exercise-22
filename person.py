cf8ce401d3b129c77a950040a2cdd956bddbfb51
Anssi Y-L <anssi.ylileppala@brightstraining.com>
2022-10-24 13:42:06 +0300

    Created person class

---
+++ person.py
@@ -0,0 +1,23 @@
+class Person:
+    def __init__(self, name: str, age: int):
+        self.__name = name
+        self.__age = age
+
+    @property
+    def name(self):
+        return self.__name
+
+    @name.setter
+    def name(self, name):
+        self.__name = name
+
+    @property
+    def age(self):
+        return self.__age
+
+    @age.setter
+    def age(self, age):
+        self.__age = age
+
+    def __str__(self):
+        return f"Person name is {self.__name} and age is {self.__age}."
